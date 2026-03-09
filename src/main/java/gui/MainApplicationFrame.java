c35cb897ee242f5da89c67d5cef9b520079507f5
andrey-miheev <andreymikheyev2006@gmail.com>
2026-03-09 20:56:44 +0500

    Robot with first_task

---
+++ src/main/java/gui/MainApplicationFrame.java
@@ -0,0 +1,219 @@
+package gui;
+
+import log.Logger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Главное окно приложения
+ */
+public class MainApplicationFrame extends JFrame
+{
+    private final JDesktopPane desktopPane = new JDesktopPane();
+
+    /**
+     * Конструктор главного окна
+     */
+    public MainApplicationFrame() {
+        //Make the big window be indented 50 pixels from each edge
+        //of the screen.
+        int inset = 50;        
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds(inset, inset,
+            screenSize.width  - inset*2,
+            screenSize.height - inset*2);
+
+        setContentPane(desktopPane);
+        
+        
+        LogWindow logWindow = createLogWindow();
+        addWindow(logWindow);
+
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.setSize(400,  400);
+        addWindow(gameWindow);
+
+        setJMenuBar(generateMenuBar());
+        setupWindowClosingHandler();
+    }
+
+    /**
+     * Обработчик события закрытия окна с показом диалога подтверждения
+     */
+    private void setupWindowClosingHandler(){
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                confirmExit();
+            }
+        });
+    }
+
+    /**
+     * Показывает диалог подтверждения выхода
+     */
+    private void confirmExit(){
+        int result = JOptionPane.showConfirmDialog(
+                this,
+                "Вы действительно хотите выйти из приложения?",
+                "Подтверждение выхода",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
+
+        if (result == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Создает окно лога
+     * Инициализирует окно с источником логов и добавляет тестовое сообщение
+     */
+    protected LogWindow createLogWindow()
+    {
+        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+        logWindow.setLocation(10,10);
+        logWindow.setSize(300, 800);
+        setMinimumSize(logWindow.getSize());
+        logWindow.pack();
+        Logger.debug("Протокол работает");
+        return logWindow;
+    }
+
+    /**
+     * Добавляет внутреннее окно в рабочую область
+     * @param frame внутреннее окно для добавления
+     */
+    protected void addWindow(JInternalFrame frame)
+    {
+        desktopPane.add(frame);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Создает главное меню
+     */
+    private JMenuBar generateMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+
+        menuBar.add(createFileMenu());
+        menuBar.add(createLookAndFeelMenu());
+        menuBar.add(createTestMenu());
+
+        return menuBar;
+    }
+
+    /**
+     * Создает выпадающее меню "Файл"
+     */
+    private JMenu createFileMenu(){
+        JMenu menu = new JMenu("Файл");
+        menu.setMnemonic(KeyEvent.VK_F);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Управление файлами и приложением");
+        menu.add(createExitMenuItem());
+
+        return menu;
+    }
+
+    /**
+     * Создает пункт меню для выхода из приложения
+     */
+    private JMenuItem createExitMenuItem(){
+        JMenuItem exitItem = new JMenuItem("Выход", KeyEvent.VK_X);
+        exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.ALT_DOWN_MASK));
+        exitItem.addActionListener((event) -> {
+            Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(
+                    new WindowEvent(this, WindowEvent.WINDOW_CLOSING)
+            );
+        });
+        return exitItem;
+    }
+
+    /**
+     * Создает меню выбора режима отображения
+     */
+    private JMenu createLookAndFeelMenu(){
+        JMenu menu = new JMenu("Режим отображения");
+        menu.setMnemonic(KeyEvent.VK_V);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Управление режимом отображения приложения");
+
+        menu.add(createSystemLookAndFeelItem());
+        menu.add(createCrossPlatformLookAndFeelItem());
+
+        return menu;
+    }
+
+    /**
+     *  Создает пункт системная схема
+     */
+    private JMenuItem createSystemLookAndFeelItem(){
+        JMenuItem item = new JMenuItem("Системная схема", KeyEvent.VK_S);
+        item.addActionListener((event) -> {
+            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            this.invalidate();
+        });
+        return item;
+    }
+
+    /**
+     * Создает пункт универсальная схема
+     */
+    private JMenuItem createCrossPlatformLookAndFeelItem(){
+        JMenuItem item = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
+        item.addActionListener((event) -> {
+            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            this.invalidate();
+        });
+        return item;
+    }
+
+    /**
+     * Создает меню тестовых команд
+     */
+    private JMenu createTestMenu(){
+        JMenu menu = new JMenu("Тесты");
+        menu.setMnemonic(KeyEvent.VK_T);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Тестовые команды");
+        menu.add(createAddLogMessageItem());
+
+        return menu;
+    }
+
+    /**
+     * Создает пункт для добавления сообщения в лог
+     */
+    private JMenuItem createAddLogMessageItem(){
+        JMenuItem item = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
+        item.addActionListener((event) -> {
+            Logger.debug("Новая строка");
+        });
+        return item;
+    }
+
+    /**
+     * Устанавливает LookAndFeel приложения
+     * @param className полное имя класса
+     */
+    private void setLookAndFeel(String className)
+    {
+        try
+        {
+            UIManager.setLookAndFeel(className);
+            SwingUtilities.updateComponentTreeUI(this);
+        }
+        catch (ClassNotFoundException | InstantiationException
+            | IllegalAccessException | UnsupportedLookAndFeelException e)
+        {
+            // just ignore
+        }
+    }
+}
