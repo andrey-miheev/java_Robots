82663c7e95f0437e46ae53345b9cfd7f3b8b185c
andrey-miheev <andreymikheyev2006@gmail.com>
2026-04-24 00:19:57 +0500

    Изменен Visualizer, теперь отвечает только за отрисовку состояния модели

--- src/main/java/gui/GameVisualizer.java
+++ src/main/java/gui/GameVisualizer.java
@@ -2,202 +2,93 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class GameVisualizer extends JPanel
+/**
+ * Класс Представления для игрового поля
+ * Отвечает только за отрисовку состояния модели
+ */
+public class GameVisualizer extends JPanel implements PropertyChangeListener
 {
-    private final Timer timer = initTimer();
-    
-    private static Timer initTimer() 
-    {
-        Timer timer = new Timer("events generator", true);
-        return timer;
-    }
-    
-    private volatile double robotPositionX = 100;
-    private volatile double robotPositionY = 100; 
-    private volatile double robotDirection = 0; 
+    private final RobotModel model;
 
-    private volatile int targetPositionX = 150;
-    private volatile int targetPositionY = 100;
-    
-    private static final double MAX_VELOCITY = 0.1;
-    private static final double MAX_ANGULAR_VELOCITY = 0.001;
-    
-    public GameVisualizer() 
+    public GameVisualizer(RobotModel model)
     {
-        timer.schedule(new TimerTask()
-        {
-            @Override
-            public void run()
-            {
-                onRedrawEvent();
-            }
-        }, 0, 50);
-        timer.schedule(new TimerTask()
-        {
-            @Override
-            public void run()
-            {
-                onModelUpdateEvent();
-            }
-        }, 0, 10);
-        addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mouseClicked(MouseEvent e)
-            {
-                setTargetPosition(e.getPoint());
-                repaint();
-            }
-        });
+        this.model = model;
+        this.model.addPropertyChangeListener(this);
         setDoubleBuffered(true);
     }
 
-    protected void setTargetPosition(Point p)
-    {
-        targetPositionX = p.x;
-        targetPositionY = p.y;
-    }
-    
-    protected void onRedrawEvent()
-    {
-        EventQueue.invokeLater(this::repaint);
+    /**
+     * Метод вызывается автоматически, когда модель оповещает об изменениях
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        repaint();
     }
 
-    private static double distance(double x1, double y1, double x2, double y2)
-    {
-        double diffX = x1 - x2;
-        double diffY = y1 - y2;
-        return Math.sqrt(diffX * diffX + diffY * diffY);
-    }
-    
-    private static double angleTo(double fromX, double fromY, double toX, double toY)
-    {
-        double diffX = toX - fromX;
-        double diffY = toY - fromY;
-        
-        return asNormalizedRadians(Math.atan2(diffY, diffX));
-    }
-    
-    protected void onModelUpdateEvent()
-    {
-        double distance = distance(targetPositionX, targetPositionY, 
-            robotPositionX, robotPositionY);
-        if (distance < 0.5)
-        {
-            return;
-        }
-        double velocity = MAX_VELOCITY;
-        double angleToTarget = angleTo(robotPositionX, robotPositionY,
-                targetPositionX, targetPositionY);
-        double angularVelocity = 0;
-        if (angleToTarget > robotDirection)
-        {
-            angularVelocity = MAX_ANGULAR_VELOCITY;
-        }
-        if (angleToTarget < robotDirection)
-        {
-            angularVelocity = -MAX_ANGULAR_VELOCITY;
-        }
-        
-        moveRobot(velocity, angularVelocity, 10);
-    }
-    
-    private static double applyLimits(double value, double min, double max)
-    {
-        if (value < min)
-            return min;
-        if (value > max)
-            return max;
-        return value;
-    }
-    
-    private void moveRobot(double velocity, double angularVelocity, double duration)
-    {
-        velocity = applyLimits(velocity, 0, MAX_VELOCITY);
-        angularVelocity = applyLimits(angularVelocity, -MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY);
-        double newX = robotPositionX + velocity / angularVelocity * 
-            (Math.sin(robotDirection  + angularVelocity * duration) -
-                Math.sin(robotDirection));
-        if (!Double.isFinite(newX))
-        {
-            newX = robotPositionX + velocity * duration * Math.cos(robotDirection);
-        }
-        double newY = robotPositionY - velocity / angularVelocity * 
-            (Math.cos(robotDirection  + angularVelocity * duration) -
-                Math.cos(robotDirection));
-        if (!Double.isFinite(newY))
-        {
-            newY = robotPositionY + velocity * duration * Math.sin(robotDirection);
-        }
-        robotPositionX = newX;
-        robotPositionY = newY;
-        double newDirection = asNormalizedRadians(robotDirection + angularVelocity * duration); 
-        robotDirection = newDirection;
-    }
-
-    private static double asNormalizedRadians(double angle)
-    {
-        while (angle < 0)
-        {
-            angle += 2*Math.PI;
-        }
-        while (angle >= 2*Math.PI)
-        {
-            angle -= 2*Math.PI;
-        }
-        return angle;
+    /**
+     * Отрисовывает игровые объекты - робот, цель
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        drawRobot(g2d,
+                round(model.getX()),
+                round(model.getY()),
+                model.getDirection()
+        );
+        drawTarget(g2d, model.getTargetX(), model.getTargetY());
     }
     
     private static int round(double value)
     {
         return (int)(value + 0.5);
     }
-    
-    @Override
-    public void paint(Graphics g)
-    {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D)g; 
-        drawRobot(g2d, round(robotPositionX), round(robotPositionY), robotDirection);
-        drawTarget(g2d, targetPositionX, targetPositionY);
-    }
-    
+
+    /**
+     * Рисует закрашенный овал, центрированный в указанной точке
+     */
     private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
     {
         g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
-    
+
+    /**
+     * Рисует контур овала, центрированного в указанной точке
+     */
     private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
     {
         g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
-    
+
+    /**
+     * Рисует тело робота
+     */
     private void drawRobot(Graphics2D g, int x, int y, double direction)
     {
-        int robotCenterX = round(robotPositionX); 
-        int robotCenterY = round(robotPositionY);
-        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY); 
+        AffineTransform t = AffineTransform.getRotateInstance(direction, x, y);
         g.setTransform(t);
+
         g.setColor(Color.MAGENTA);
-        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+        fillOval(g, x, y, 30, 10);
         g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+        drawOval(g, x, y, 30, 10);
         g.setColor(Color.WHITE);
-        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        fillOval(g, x  + 10, y, 5, 5);
         g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        drawOval(g, x  + 10, y, 5, 5);
     }
-    
+
+    /**
+     * Рисует цель на поле
+     */
     private void drawTarget(Graphics2D g, int x, int y)
     {
-        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0); 
-        g.setTransform(t);
+        g.setTransform(new AffineTransform());
         g.setColor(Color.GREEN);
         fillOval(g, x, y, 5, 5);
         g.setColor(Color.BLACK);
